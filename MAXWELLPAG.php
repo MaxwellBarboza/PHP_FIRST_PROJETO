31779d1fb2290f81886624cf23ca03fbbd09d298
Maxwell <maxwellaugustomb@hotmail.com>
2022-07-11 16:22:16 -0300

    Primeiro Projeto

---
+++ MAXWELLPAG.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Maxwell Barboza</title>
+	<link href="Stylecss.css" rel="stylesheet">
+  </head>
+  <body>
+   <header>
+   <img src="MAXPIC.jpeg" alt="Ilustração Max" class="photo" />
+    <h1 id="title"> Maxwell Barboza</h1>
+   </header>
+   <section>
+    <header>
+	 <h2 class="subtitle">Posts</h2>
+	</header>
+    <article class="post">
+     <header>
+      <h3 class="post_title">Post #1</h3>
+	  <img src="https://uploads.onsize.com.br/cndl/varejosa/2021/03/08013726/Transporte_y_Logistica_.jpg" class="post_image" alt ="Editor de texto imagem logística" />
+     </header>
+	 <p class="post_conent">
+	 Olá, me chamo Maxwell Barboza e sou um profissional de gestão, amante de tecnologia como pode ser visto em meu currículo no linkedin. <a href="https://www.linkedin.com/in/maxwell-augusto/"target="_blank"> LINKEDIN MAXWELL BARBOZA </a> . Estou inciando meu trabalho como DEV e está sendo uma experiência incrível, para me contatar pode me enviar um e-mail é só clicar no link. <a href="mailto:maxwellaugustomb@hotmail.com"> e-mail de MAXWELL BARBOZA </a>, estou muito intusiasmado com essa jornada e tenho certeza que irá me proporcionar um crescimento profissional e pessoal imenso, e um olhar tech que ainda não havia desenvolvido. Para verificar meu portifólio de projetos basta clicar no meu GITHUB no link no rodapé.
+	 </p>
+    </article>	 
+   </section>
+   <footer>
+   <ul class="contacts_list">
+     <li>
+	   <a href="mailto:maxwellaugustomb@hotmail">maxwellaugustomb@hotmail</a>
+	 </li>
+	 <li>
+	   <a href="https://www.linkedin.com/in/maxwell-augusto/" target="_blank">Linkedin</a>
+	 </li>
+	 <li>
+       <a href="https://github.com/MaxwellBarboza" target="_blank">Github</a> 
+	 </li>
+   </footer>
+  </body>
+</html>  
---
+++ Página.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Maxwell Barboza</title>
+	<link ref="stylesheet" href="style.css">
+  </head>
+  <body>
+   <header>
+   <IMG SRC="img/c:\Users\LDB-NB-5919\Desktop\IMAGEM" alt="Ilustração Max e Mona">
+    <h1 id="title"> Maxwell Barboza</h1>
+   </header>
+   <section>
+    <header>
+	 <h2 class="subtitle">Posts</h2>
+	</header>
+    <article class="post">
+     <header>
+      <h3 class="post_title">Post #1</h3>
+	  <img src="https://www.google.com.br/search?q=logistica&tbm=isch&sxsrf=ALiCzsazGBaIp5YsIslBDsRagz2CO03lGA:1657134757796&source=lnms&sa=X&ved=2ahUKEwjv_9SE_OT4AhWEIrkGHR-dDy0Q_AUoAnoECAEQBA&biw=1366&bih=643&dpr=1#imgrc=mXFm3GpbJmKylM" alt ="Editor de texto imagem logística">
+     </header>
+	 <p>
+	 Lorem Ipsum is simply dummy text of the printing and typesetting industry. <a href="https://www.globo.com/"target="_blank"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</a> , when an unknown printer took a galley of type and scrambled it to make a type specimen book. <a href="maxwellaugustomb@hotmail.com">It has survived not only five centuries</a>, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+	 </p>
+    </article>	 
+   </section>
+   <footer>
+   <ul>
+     <li>
+	   <a href="mailto:maxwellaugustomb@hotmail">maxwellaugustomb@hotmail</a>
+	 </li>
+	 <li>
+	   <a href="https://www.linkedin.com/in/maxwell-augusto/" target="_blank">Linkedin</a>
+	 </li>
+	 <li>
+       <a href="https://github.com/MaxwellBarboza" target="_blank">Github</a> 
+	 </li>
+   </footer>
+  </body>
+</html>  
